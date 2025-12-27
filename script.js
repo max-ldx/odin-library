051f035e24e03a9ae9e4916acc2e4a7fbdda459c
max-ldx <242964282+max-ldx@users.noreply.github.com>
2025-12-27 10:22:49 +0100

    refactor: display library function

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const myLibrary = [];
 
+addBookToLibrary({ title: 'Test', author: 'Test', pages: 250, read: true })
+
+displayBooks(myLibrary);
+
 function Book({ title, author, pages, read = false }) {
     this.id = crypto.randomUUID();
     this.title = title;
@@ -10,4 +14,31 @@ function Book({ title, author, pages, read = false }) {
 
 function addBookToLibrary({ title, author, pages, read }) {
     myLibrary.push(new Book({ title, author, pages, read }));
+}
+
+function displayBooks(library) {
+    const libraryElement = document.querySelector('.library');
+
+    for (const book of library) {
+        const bookElement = document.createElement('div');
+        bookElement.classList.add('book');
+
+        const bookTitleElement = document.createElement('h2');
+        bookTitleElement.textContent = book.title;
+        bookElement.appendChild(bookTitleElement);
+
+        const bookAuthorElement = document.createElement('p');
+        bookAuthorElement.textContent = book.author;
+        bookElement.appendChild(bookAuthorElement);
+
+        const bookPagesElement = document.createElement('p');
+        bookPagesElement.textContent = `${book.pages} pages`;
+        bookElement.appendChild(bookPagesElement);
+
+        const bookReadElement = document.createElement('p');
+        bookReadElement.textContent = book.read ? 'Read' : 'To read';
+        bookElement.appendChild(bookReadElement);
+
+        libraryElement.appendChild(bookElement);
+    }
 }
