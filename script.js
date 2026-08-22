25baf217ffa34980e8060462d4a01e2ee15cd235
max-ldx <242964282+max-ldx@users.noreply.github.com>
2026-08-22 16:35:21 +0200

    Add display books function

--- script.js
+++ script.js
@@ -16,3 +16,33 @@ function addBookToLibrary(title, author, pages, read) {
     const book = new Book(title, author, pages, read);
     library.push(book);
 }
+
+function displayBooks() {
+    const libraryEl = document.querySelector("#library");
+
+    for (const book of library) {
+        const bookEl = document.createElement("div");
+        const titleEl = document.createElement("h2");
+        const authorEl = document.createElement("p");
+        const pagesEl = document.createElement("p");
+        const readEl = document.createElement("p");
+
+        bookEl.classList.add("book");
+
+        bookEl.appendChild(titleEl);
+        bookEl.appendChild(authorEl);
+        bookEl.appendChild(pagesEl);
+        bookEl.appendChild(readEl);
+
+        titleEl.textContent = book.title;
+        authorEl.textContent = book.author;
+        pagesEl.textContent = `${book.pages} p.`;
+        readEl.textContent = book.read ? "Yes" : "No";
+
+        libraryEl.appendChild(bookEl);
+    }
+}
+
+addBookToLibrary("Ars Obscura : Sorcier d'Empire", "François Barranger", 496, true);
+addBookToLibrary("La Quête d'Ewilan : D'un Monde à l'Autre", "Pierre Bottero", 281, true);
+displayBooks();
