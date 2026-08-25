9f173a67908e1df79df80b4d2d6b9e1afe43e90e
max-ldx <242964282+max-ldx@users.noreply.github.com>
2026-08-25 14:38:44 +0200

    Add new book form

--- script.js
+++ script.js
@@ -19,8 +19,9 @@ function addBookToLibrary(title, author, pages, read) {
 
 function displayBooks() {
     const libraryEl = document.querySelector("#library");
+    libraryEl.textContent = null;
 
-    for (const book of library) {
+    for (const book of library.toReversed()) {
         const bookEl = document.createElement("div");
         const titleEl = document.createElement("h2");
         const authorEl = document.createElement("p");
@@ -43,6 +44,39 @@ function displayBooks() {
     }
 }
 
+function setupForm() {
+    const dialog = document.querySelector("dialog");
+    const form = document.querySelector("#new-book-form");
+
+    form.addEventListener("submit", event => {
+        event.preventDefault();
+        const formData = new FormData(form);
+        const entries = Object.fromEntries(formData);
+        console.log(entries);
+
+        addBookToLibrary(entries.title, entries.author, entries.pages, entries.read === undefined ? false : true)
+        displayBooks();
+        dialog.close();
+    });
+}
+
+function setupModalOpen() {
+    const newBookButton = document.querySelector("#new-book-button");
+    const dialog = document.querySelector("dialog");
+
+    newBookButton.addEventListener("click", () => dialog.showModal());
+}
+
+function setupModalClose() {
+    const closeDialogButton = document.querySelector("#dialog-close-button");
+    const dialog = document.querySelector("dialog");
+
+    closeDialogButton.addEventListener("click", () => dialog.close());
+}
+
+setupModalOpen();
+setupModalClose();
+setupForm();
 addBookToLibrary("Ars Obscura : Sorcier d'Empire", "François Barranger", 496, true);
 addBookToLibrary("La Quête d'Ewilan : D'un Monde à l'Autre", "Pierre Bottero", 281, true);
 displayBooks();
